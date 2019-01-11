80086babce4d4c956ce659d95e63c34e2841f835
Aitor Hernández <alu0100987522@gmail.com>
2019-01-11 10:09:35 +0000

    Práctica 12: To_s del menú implementado.

--- lib/gema/menu.rb
+++ lib/gema/menu.rb
@@ -18,11 +18,59 @@ class Menu
         end
     end
     
+    def to_s
+        output = @nombre
+        output << "\t\t \"#{@titulo}\""
+        output << "\n#{'=' * 50 }\n"
+        
+        
+        output
+    end
+    
+    def titulo(text)
+        @titulo = text
+    end
 
-    
-    
-    
+    def ingesta(intervalo = {})
+        @ingesta << " (#{intervalo[:min]})" if intervalo[:min]
+        @ingesta << " (#{intervalo[:max]})" if intervalo[:max]
+    end
 
+    def desayuno(alimento, composicion = {})
+        grasas = composicion[:grasas] if composicion[:grasas]
+        gsat = composicion[:gsat] if composicion[:gsat]
+        carbh = composicion[:carbh] if composicion[:carbh]
+        azucar = composicion[:azucar] if composicion[:azucar]
+        protein = composicion[:protein] if composicion[:protein]
+        sal = composicion[:sal] if composicion[:sal]
+        
+        @desayuno << Etiqueta.new(alimento, grasas, gsat, carbh, azucar, protein, sal)
+        
+    end
+    
+    def almuerzo(alimento, composicion = {})
+        grasas = composicion[:grasas] if composicion[:grasas]
+        gsat = composicion[:gsat] if composicion[:gsat]
+        carbh = composicion[:carbh] if composicion[:carbh]
+        azucar = composicion[:azucar] if composicion[:azucar]
+        protein = composicion[:protein] if composicion[:protein]
+        sal = composicion[:sal] if composicion[:sal]
+        
+        @almuerzo << Etiqueta.new(alimento, grasas, gsat, carbh, azucar, protein, sal)
+        
+    end
+    
+    def cena(alimento, composicion = {})
+        grasas = composicion[:grasas] if composicion[:grasas]
+        gsat = composicion[:gsat] if composicion[:gsat]
+        carbh = composicion[:carbh] if composicion[:carbh]
+        azucar = composicion[:azucar] if composicion[:azucar]
+        protein = composicion[:protein] if composicion[:protein]
+        sal = composicion[:sal] if composicion[:sal]
+        
+        @cena << Etiqueta.new(alimento, grasas, gsat, carbh, azucar, protein, sal)
+        
+    end
     
 end
 
--- spec/p12_spec.rb
+++ spec/p12_spec.rb
@@ -4,7 +4,43 @@ RSpec.describe Menu do
     
     before :all do
         @menu1 = Menu.new("Lunes") do
-            
+            titulo      "Día ligero."
+            ingesta     :min => 35, :max =>40
+            desayuno    "Tostada pan blanco.", 
+                        :grasas => 4.0,
+                        :gsat => 0.6, 
+                        :carbh => 54.4, 
+                        :azucar => 4.7,
+                        :protein => 9.0,
+                        :sal => 1.1
+            desayuno    "Batido de chocolate.",
+                        :grasas => 10.4,
+                        :gsat => 6.5, 
+                        :carbh => 57.3, 
+                        :azucar => 52.0,
+                        :protein => 9.2,
+                        :sal => 1.5
+            almuerzo    "Pure de papas.",
+                        :grasas => 6.8,
+                        :gsat => 1.4, 
+                        :carbh => 35.4, 
+                        :azucar => 1.2,
+                        :protein => 4.0,
+                        :sal => 1.0
+            almuerzo    "Gazpacho.",
+                        :grasas => 0.2,
+                        :gsat => 0.07, 
+                        :carbh => 4.5, 
+                        :azucar => 1.4,
+                        :protein => 7.1,
+                        :sal => 0.6
+            cena        "Ensalada de rúcula.",
+                        :grasas => 0.1,
+                        :gsat => 0.05, 
+                        :carbh => 0.7, 
+                        :azucar => 0.4,
+                        :protein => 0.5,
+                        :sal => 0.7
         end
     end
     
